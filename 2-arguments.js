3117c9c3155bdca97f900a04d6a70a4ed84f77c8
Peter <peterogb067@gmail.com>
2025-07-13 17:09:14 +0100

    Add script to print message based on arguments

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+// File: 2-arguments.js
+
+const argCount = Process.argv.length - 2;
+
+if (argCount === 0) {
+    console.log("No argument");
+} else if (argCount === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Argument found");
+}
